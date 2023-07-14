e34820da92ed7cb723c8d9a897c63ef5abd44511
Raaina <raaina.daniar@gmail.com>
2023-07-14 10:24:47 -0400

    I changed world to scholars, changed font, and changed the color

--- github example/ContentView.swift
+++ github example/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, scholars!")
+                .font(.title)
+                .foregroundColor(Color.orange)
         }
         .padding()
     }
