b459b54f72e86577aba3d02e59e0967febba768a
Raaina <raaina.daniar@gmail.com>
2023-07-14 10:17:31 -0400

    Initial Commit

---
+++ github example/github_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_exampleApp.swift
+//  github example
+//
+//  Created by Mohamad Daniar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct github_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
